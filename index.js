53592c6e729633b2a832237d49bb9da13efefbb0
kohei.akita <kohei.akita@photosynth.co.jp>
2017-08-27 18:33:43 +0900

    modified typo

--- index.js
+++ index.js
@@ -245,11 +245,11 @@ WpaCLI.prototype.addNetwork = function (params, cb) {
 };
 
 WpaCLI.prototype.removeNetwork = function (netId, cb) {
-    this.request('REMOVE_NETOWRK ' + netId, cb);
+    this.request('REMOVE_NETWORK ' + netId, cb);
 };
 
 WpaCLI.prototype.disableNetwork = function (netId, cb) {
-    this.request('DISABLE_NETOWRK ' + netId, cb);
+    this.request('DISABLE_NETWORK ' + netId, cb);
 };
 
 WpaCLI.prototype.enableNetwork = function (netId, cb) {
